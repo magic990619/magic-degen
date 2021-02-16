85d5f4cc9ac007b8ab025376a5c2b820b9dff653
Magic Dev <magicrjh0619@gmail.com>
2021-02-16 17:32:23 +0100

    Add loading indicator

--- src/views/Asset.vue
+++ src/views/Asset.vue
@@ -27,13 +27,12 @@
       <Space size="md" />
 
       <div>
-        <div class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-yellow-100 text-yellow-800">
+        <div class="inline-flex items-center">
+          <!--
           <h3 class="text-lg leading-6 font-medium text-gray-900">
             {{ hasFetched ? "Ethereum + ERC20 tx stats" : "Loading off-chain data (This might take a while if you have a lot of transactions)" }}
           </h3>
-
-          <Space size="md" />
-
+          -->
           <!-- Enabled: "bg-blue", Not Enabled: "bg-gray-200" -->
           <button
             type="button"
@@ -78,9 +77,28 @@
               </span>
             </span>
           </button>
-          <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-yellow-100 text-yellow-800">
+
+          <Space size="sm" />
+
+          <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-yellow-400 text-yellow-800">
             {{ badgeState }}
           </span>
+
+          <Space size="sm" />
+
+          <svg
+            :class="hasFetched ? 'hidden -ml-1 mr-3 h-5 w-5 text-black' : 'animate-spin -ml-1 mr-3 h-5 w-5 text-black'"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
         </div>
 
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
