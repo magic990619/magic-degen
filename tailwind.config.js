4667e4b1842a73ee6ce72a73110ea1bb3f428016
Magic Dev <magicrjh0619@gmail.com>
2021-02-16 09:22:37 +0100

    Add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: "var(--primary)",
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      indigo: colors.indigo,
+      red: colors.red,
+      green: colors.green,
+      blue: "#6799e5",
+      yellow: colors.yellow,
+    },
     boxShadow: {
       "3": "3px 3px 0 0 rgba(0, 0, 0, 0.06)",
     },
