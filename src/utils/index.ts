f0ab1dee8d520cdcbd3662ccb051a367966cbb9c
Magic Dev <magicrjh0619@gmail.com>
2021-02-20 00:25:58 +0100

    Move JsonTxResult Interface

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -16,30 +16,6 @@ import { WETH, DAI, EMPFEB, EMPMAR } from "./addresses";
 dayjs.extend(utc);
 dayjs.extend(duration);
 
-interface JsonTxResult {
-  blockHash: string;
-  blockNumber: string;
-  confirmations: string;
-  contractAddress: string;
-  cumulativeGasUsed: string;
-  from: string;
-  gas: string;
-  gasPrice: string;
-  gasUsed: string;
-  hash: string;
-  input: string;
-  isError: string;
-  nonce: string;
-  timeStamp: string;
-  to: string;
-  tokenDecimal: string;
-  tokenName: string;
-  tokenSymbol: string;
-  transactionIndex: string;
-  txreceipt_status: string;
-  value: string;
-}
-
 export function stateSave(key, state) {
   window.localStorage.setItem(key, JSON.stringify(state));
 }
