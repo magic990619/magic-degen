f78d34c859e019d1dc6f2aa43251fa4683f41227
Magic Dev <magicrjh0619@gmail.com>
2021-04-19 13:22:12 +0200

    Fix type bug

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -629,7 +629,7 @@ export const getCurrentTWAP = async (poolAddress) => {
     return -1;
   }
 
-  const currentTWAP = new BigNumber(data.price).decimalPlaces(data.roundingDecimals);
+  const currentTWAP = new BigNumber(data.price).decimalPlaces(parseInt(data.price));
   
   return currentTWAP;
 };
